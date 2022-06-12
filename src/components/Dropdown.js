dc834e70ddce9e1e2fa6105aee1d5bc05cb4035a
ClaytonDewey <clay@dryadmedia.com>
2022-06-12 14:27:15 -0500

    Add label prop to make dropdown reusable

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef();
 
@@ -37,23 +37,21 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <React.Fragment>
-      <div ref={ref} className="ui form">
-        <div className="field">
-          <label className="label">Select a Color</label>
-          <div
-            onClick={() => setOpen(!open)}
-            className={`ui selection dropdown ${open ? "visible active" : ""}`}
-          >
-            <i className="dropdown icon"></i>
-            <div className="text">{selected.label}</div>
-            <div className={`menu ${open ? "visible transition" : ""}`}>
-              {renderedOptions}
-            </div>
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label className="label">{label}</label>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
