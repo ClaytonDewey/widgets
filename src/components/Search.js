0910e983defb1e32c6334b57bab4d1c4147681bb
ClaytonDewey <clay@dryadmedia.com>
2022-06-12 09:55:46 -0500

    Set up the rendered results

--- src/components/Search.js
+++ src/components/Search.js
@@ -25,6 +25,25 @@ const Search = () => {
     }
   }, [term]);
 
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui form">
@@ -38,6 +57,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
