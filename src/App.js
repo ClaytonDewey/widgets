36838e302047f6edb150d635461d5542a96084d0
ClaytonDewey <clay@dryadmedia.com>
2022-06-13 09:54:53 -0500

    Create & import Header component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -38,7 +39,8 @@ const options = [
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
   return (
-    <div>
+    <div className="ui container">
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
